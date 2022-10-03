331695875d4d135e88025c9142046d571a7b0cb7
Christian Rémillard <christian.remillard@gmail.com>
2022-10-03 12:15:42 -0400

    Restored deleted changes.

--- packages/harvester/lib/harvester/default-options.js
+++ packages/harvester/lib/harvester/default-options.js
@@ -1,14 +1,101 @@
+import { tmpdir } from 'node:os'
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path'
 
-const pkg = JSON.parse(await readFile(new URL('../../package.json', import.meta.url)))
-const tmpDir = join(process.env.TEMP, 'corvee');
+const pkg = JSON.parse(await readFile(new URL('../../package.json', import.meta.url), 'utf-8'))
+const tmpDir = join(process.env.TEMP || tmpdir(), 'corvee');
 
-/*
+/**
  * Corvée options
+ * @typedef
  */
 
 export const defaultHarvesterOptions = {
+
+    //
+    // Intern options. Do not use
+    //
+
+    /**
+     * Wether to use defaultHarvesterOptions.ignoreDefaults or not
+     * @private
+     */
+    useIgnoreDefaults: true,
+
+    /**
+     * Wether to use defaultHarvesterOptions.schemesDefaults or not
+     * @private
+     */
+    useSchemesDefaults: true,
+
+    /**
+     * Wether to use defaultHarvesterOptions.noFollowDefaults or not
+     * @private
+     */
+    useNoFollowDefaults: true,
+
+    //
+    // Public options
+    //
+
+    /**
+     * URL patterns that will be blocked from external requests
+     * @type {Array.<string>}
+     */
+    blockRequestsFromUrlPatterns: ['.bmp', '.css', '.cur', '.gif', '.gzip', '.jpeg', '.jpg', '.mp4', '.png', '.svg', '.ttf', '.webp', '.woff', '.woff2', '.zip', 'googleadservices.com'],
+
+    /**
+     * Which browser to use
+     * @type {'chromium' | 'firefox' | 'webkit'} browserType
+     */
+    browser: 'chromium',
+
+    /**
+     * Wether to check extern links or not
+     */
+    checkExtern: true,
+    fetchLinksOnce: true,
+    getPerfData: false,
+
+    /**
+     * URLs matching the given strings / regular expression will be ignored and not checked.
+     * @type {Array<string|RegExp>}
+     */
+    ignore: [],
+
+    /**
+     * @type {Array<string|RegExp>}
+     */
+    ignoreDefaults: ['www.google-analytics.com', '/gtag/js', 'ga.js', 'analytics.js', 'https://www.googleadservices.com/', 'doubleclick.net'],
+
+    /**
+     * Array of URLs matching the given strings / regular expressions. These URLs define the scope of the crawling ("intern" links)
+     * @type {Array.<string|RegExp>}
+     */
+    internLinks: [],
+    linkParserDelay: 0,
+    logLevel: 'info',
+    maxDepth: Infinity,
+    navigationOnly: true,
+
+    /**
+     * Check but do not recurse into URLs matching the given strings / regular expressions.
+     * @type {Array<string|RegExp>}
+     */
+    noFollow: [],
+
+    /**
+     * @type {Array<string|RegExp>}
+     */
+    noFollowDefaults: [],
+
+    /**
+     * @type {null | Function}
+     */
+    normalizeUrlFunction: null,
+    notify: true,
+    notifyDelay: 10000,
+    notifyLogLevel: 'info',
 
     /**
      * @typedef {'domcontentloaded' | 'load' | 'networkidle'} WaitUntilProperty
@@ -19,75 +106,29 @@ export const defaultHarvesterOptions = {
      * @typedef { { intern: WaitUntilProperty, extern: WaitUntilProperty } | WaitUntilProperty } WaitUntilType
      */
 
-    //
-    // Intern options. Do not use
-    //
-
-    /*
-     * Wether to use defaultHarvesterOptions.ignoreDefaults or not
-     * @private
-     */
-    useIgnoreDefaults: true,
-
-    /*
-     * Wether to use defaultHarvesterOptions.schemesDefaults or not
-     * @private
-     */
-    useSchemesDefaults: true,
-
-    /*
-     * Wether to use defaultHarvesterOptions.noFollowDefaults or not
-     * @private
-     */
-    useNoFollowDefaults: true,
-
-    //
-    // Public options
-    //
-
-    // URL patterns that will be blocked from external requests
-    blockRequestsFromUrlPatterns: ['.bmp', '.css', '.cur', '.gif', '.gzip', '.jpeg', '.jpg', '.mp4', '.png', '.svg', '.ttf', '.webp', '.woff', '.woff2', '.zip', 'googleadservices.com'],
-    /*
-     * Which browser to use
-     * @type {'chromium' | 'firefox' | 'webkit'} browserType
-    */
-    browser: 'chromium',
-    // Wether to check extern links or not
-    checkExtern: true,
-    fetchLinksOnce: true,
-    getPerfData: false,
-    // URLs matching the given strings / regular expression will be ignored and not checked.
-    ignore: [],
-    ignoreDefaults: ['www.google-analytics.com', '/gtag/js', 'ga.js', 'analytics.js', 'https://www.googleadservices.com/', 'doubleclick.net'],
-    // Array of URLs matching the given strings / regular expressions. These URLs define the scope of the crawling ("intern" links)
-    internLinks: [],
-    linkParserDelay: 0,
-    logLevel: 'info',
-    maxDepth: Infinity,
-    navigationOnly: true,
-    // Check but do not recurse into URLs matching the given strings / regular expressions.
-    noFollow: [],
-    noFollowDefaults: [],
-    normalizeUrlFunction: null,
-    notify: true,
-    notifyDelay: 10000,
-    notifyLogLevel: 'info',
-
-    /*
+    /**
      * Wait for the page event before consider operation succeeded.
-     * @property {WaitUntilType} pageWaitUntil
+     * @type {WaitUntilType} pageWaitUntil
      */
     pageWaitUntil: {
         intern: 'domcontentloaded',
         extern: 'load'
     },
 
-    /*
+    /**
      * Proxy server URLs to use
-     * @type {string | string[]}
-    */
+     * @type {string | string[] | undefined}
+     */
     proxyUrls: undefined,
+
+    /**
+     * @type {Array<string>} config.schemes - What schemes to follow
+     */
     schemes: [],
+
+    /**
+     * @type {Array<string>}
+     */
     schemesDefaults: ['corvee', 'http', 'https'],
     startUrl: null,
     storageDir: join(tmpDir, '.storage'),
@@ -95,6 +136,9 @@ export const defaultHarvesterOptions = {
     waitInterval: 50,
 }
 
+/**
+ * @typedef
+ */
 export const defaultPlaywrightCrawlerOptions = {
     headless: true,
     maxRequestRetries: 3,
@@ -105,62 +149,58 @@ export const defaultPlaywrightCrawlerOptions = {
     useSessionPool: true
 }
 
+/**
+ * @typedef
+ */
 export const defaultBrowserPoolOptions = {
-    /*
-     * @type {number}
-     */
-    maxOpenPagesPerBrowser: 10,
 
-    /*
-     * @type {number}
-     */
-    retireBrowserAfterPageCount: 30,
-
-    /*
+    /**
      * @type {boolean}
      */
     useFingerprints: true,
 
-    /*
-     * @see {link https://crawlee.dev/api/browser-pool/interface/FingerprintOptions}
+    /**
+     * @type {object | undefined}
+     * @see {@link https://crawlee.dev/api/browser-pool/interface/FingerprintOptions}
      */
     fingerprintOptions: undefined
 }
 
+/**
+ * Launch options of Playwright
+ * @typedef
+ * @see {@link https://crawlee.dev/api/playwright-crawler/interface/PlaywrightLaunchContext}
+ */
 export const defaultLaunchContextOptions = {
-    /*
-     * Launch options of Playwright
-     * @see {@link https://crawlee.dev/api/playwright-crawler/interface/PlaywrightLaunchContext}
-     */
 
-    /*
+    /**
      * @type boolean
      */
     useChrome: false,
 
-    /*
-     * @type string
+    /**
+     * @type { string | undefined }
      */
     userAgent: undefined,
 
-    /*
-     * @type string
+    /**
+     * @type { string | undefined }
      */
     userDataDir: undefined,
 
-    /*
+    /**
      * Native Playwright options
      * @See {@link https://playwright.dev/docs/api/class-browsertype#browser-type-launch}
      */
     launchOptions: {
 
-        /*
+        /**
          * Browser launch argument options
-         * @type array
+         * @type {Array<string>}
          */
         args: ['--use-gl=egl'],
 
-        /*
+        /**
          * Puppeteer launch options
          * @type boolean
          * */
@@ -172,13 +212,13 @@ export const defaultLaunchContextOptions = {
         //  */
         // userDataDir: join(tmpDir, '.userData'),
 
-        /*
+        /**
          * The `User-Agent` HTTP header used by the browser
          * @type string
          */
         userAgent: `Mozilla/5.0 (Corvee/${pkg.version})`,
 
-        /*
+        /**
          * Browser connect options
          * @see {@link https://pptr.dev/api/puppeteer.browserconnectoptions/}
          */
@@ -191,22 +231,20 @@ export const defaultLaunchContextOptions = {
 
 }
 
-/*
+/**
  * Autoscaled pool options
+ * @typedef
  * @see {@link https://crawlee.dev/api/core/interface/AutoscaledPoolOptions}
  */
 export const defaultAutoscaledPoolOptions = {
     minConcurrency: 1,
-    /*
-     * The maximum number of tasks running in parallel.
-     */
     maxConcurrency: 5,
     scaleUpStepRatio: .05,
     scaleDownStepRatio: .05,
     maybeRunIntervalSecs: .5,
     loggingIntervalSecs: 30,
     autoscaleIntervalSecs: 10,
-    /*
+    /**
      * SystemStatus Options
      * @see {@link https://crawlee.dev/api/core/interface/SystemStatusOptions}
      */
